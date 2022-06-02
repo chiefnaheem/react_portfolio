0aaa2c820431451e74b8eccb22db922f671e2ff6
naheem adedokun <naheemadedokun21@gmail.com>
2022-06-02 22:27:14 +0100

    updates cv

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ import { SectionHeading, SectionTitle } from "../styles/SectionHeading";
 import { StyledParagraph } from "../styles/Typography.styled";
 import myImage from "../../assets/about_img.jpeg";
 import downloadIcon from "../../assets/icons/download-icon.svg";
-import resume from "../../assets/Naheem-Adedokun-CV.pdf";
+import resume from "../../assets/Naheem Adedokun-CV;.docx (3).pdf";
 
 const About = () => {
   return (
